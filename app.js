2fb72f3f78125237007288cd87a6f676ec5c94a9
d2n0s4ur <njh0625@gmail.com>
2022-09-04 20:28:24 +0900

    update: move to hashtag borard

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, DiscordAPIError, EmbedBuilder } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const fs = require('node:fs');
 const path = require('node:path');
 
+//토큰 값 파싱
 const token = process.env.DISCORD_TOKEN;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// 봇에세 권한 부여
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+
+// Command 연결
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -17,6 +23,7 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+// Event 연결
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
@@ -30,6 +37,7 @@ for (const file of eventFiles) {
 	}
 }
 
+// 커맨드를 입력하면 해당 커맨드 실행
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return ;
 
@@ -45,4 +53,44 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+// 메시지 작성 시 확인 -> 해시태그 게시판으로 가게
+client.on('messageCreate', msg => {
+  const MainchannelId = '1010496175520104490'; // Put <전체> channel's ID;
+  const hashtagchannelIds = ['1015931645070676008', '1015931665333354546']; // put hashtag channel's IDs (ex. #그림체 #노말 #피드백 #팬아트 etc ..)
+
+  if (msg.channelId != MainchannelId)
+    return ;
+
+  let mentionedchannelId = getmentionIds(msg.mentions.channels);
+  mentionedchannelId.forEach((item) => {
+    if (hashtagchannelIds.indexOf(String(item)) != -1) { // 채널 언급을 했는데, 해당 채널이 해시태크 채널인 경우
+
+      const url = `https://discord.com/channels/${msg.guildId}/${msg.channelId}}/${msg.id}`;
+
+      let embedmsg = new EmbedBuilder()
+        .setAuthor({name: msg.author.username + "#" + msg.author.discriminator, iconURL: msg.author.avatarURL(), url: url})
+        .setURL(url)
+        .setColor(0x1d7676)
+        .setTitle(`본문으로 가기`)
+        .setTimestamp()
+        .addFields(
+          { name : `message`, value: msg.content}
+        )
+        .setFooter({text: `from #${client.channels.cache.get(String(MainchannelId)).name}`, iconURL: msg.author.avatarURL()})
+
+      client.channels.cache.get(String(item)).send({embeds: [embedmsg]});
+    }
+  });
+});
+
+// Channel map 객체에서 channel의 ID를 파싱
+const getmentionIds = (channelsMap) => {
+  const channels = [];
+  channelsMap.forEach((item)=>{
+    channels.push(item.id.replace('#',''));
+  })
+  return (channels);
+};
+
+// Bot login to server
 client.login(token);
